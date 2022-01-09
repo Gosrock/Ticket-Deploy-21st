2c7a6009223d948859a4a1fd7382afb390e93167
이찬진 <water0641@naver.com>
2022-01-10 03:21:06 +0900

    push

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ if  type docker-compose > /dev/null
 then
   echo "docker-compose does not exist"
   echo "Start installing docker-compose"
-  sudo curl -L "https://github.com/docker/compose/releases/download/2.2.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
